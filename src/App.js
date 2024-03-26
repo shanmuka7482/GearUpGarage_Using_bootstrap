af35e0560a44042b1bd23ebe4bba56b5aa28b653
Shanmuka Srinivas <shanmuka srinivasa>
2024-03-26 16:18:14 +0530

    Sec_1 completed but has some bugs,sec_2 is added and has to improve

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import "./App.css";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import Header from "./components/Header.jsx";
-import Carousel from "./components/Carousel.jsx"
-import Sec1 from "./components/Sec_1.jsx"
-
+import Carousel from "./components/Carousel.jsx";
+import Sec1 from "./components/Sec_1/Sec_1.jsx";
+import Sec_2 from "./components/Sec_2.jsx"; //rafce
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Carousel/>
-      <Sec1/>
+      <Carousel />
+      <Sec1 />
+      <Sec_2 />
     </div>
   );
 }
---
+++ src/components/Sec_2.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Image from "react-bootstrap/Image";
+import bg_img from "../photos/Sec-2.png"
+
+const Sec_2 = () => {
+  return (
+    <div>
+      <Image src={bg_img} fluid />
+    </div>
+  );
+};
+
+export default Sec_2;
